f93dfc96c4e89ba340bf3265a07aeea9181ac8fa
אברהם פרנקל <avraham224@gmail.com>
2018-12-28 15:08:52 +0200

    28/12 טאב הגדרות

--- BE/Configuration.cs
+++ BE/Configuration.cs
@@ -6,10 +6,11 @@ using System.Net.Mail;
 using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.Runtime.Serialization;
+using BE.Properties;
 
 namespace BE
 {
-    public static class Configuration
+    public class Configuration
     {
         static public Random Random = new Random();
         private static int LastTestId = 10000000;
@@ -24,18 +25,147 @@ namespace BE
             return LastTestId++;
         }
 
-        // Program Setting Variables:
-        static public int MinimumTesterAge = 40;
-        static public int MaximumTestsInWeek = 40;
-        static public int MaximumTesterAge = 70;
-        static public int MinimumTaineeAge = 18;
-        static public int MinimumDaysBetweenTests = 7;
-        static public int MinimumDrivingLessons = 20;
-        static public int PassingGrade = 70;
-        static public TimeSpan TestTimeSpan = new TimeSpan(0, 30, 0);
-        static public int WorkStartHour = 7;
-        static public int WorkEndHour = 19;
-        static public int FridayWorkEndHour = 14;
+
+
+
+
+        static public int MinimumTaineeAge
+        {
+            get { return (int)Settings.Default["MinimumTaineeAge"]; }
+            set
+            {
+                if (value < 16 || value > 50)
+                    throw new Exception();
+                Settings.Default["MinimumTaineeAge"] = value;
+                Properties.Settings.Default.Save();
+            }
+        }
+
+        static public int MinimumTesterAge
+        {
+            get { return (int)Settings.Default["MinimumTesterAge"]; }
+            set
+            {
+                if (value < 18 || value > 60)
+                    throw new Exception();
+                Settings.Default["MinimumTesterAge"] = value;
+                Properties.Settings.Default.Save();
+            }
+        }
+
+        static public int MaximumTestsInWeek
+        {
+            get { return (int)Settings.Default["MaximumTestsInWeek"]; }
+            set
+            {
+                if (value < 0 || value > 99)
+                    throw new Exception();
+                Settings.Default["MaximumTestsInWeek"] = value;
+                Properties.Settings.Default.Save();
+            }
+        }
+
+        static public int MaximumTesterAge
+        {
+            get { return (int)Settings.Default["MaximumTesterAge"]; }
+            set
+            {
+                if (value < 18 || value > 120)
+                    throw new Exception();
+                Settings.Default["MaximumTesterAge"] = value;
+                Properties.Settings.Default.Save();
+            }
+        }
+
+
+
+        static public int MinimumDaysBetweenTests
+        {
+            get { return (int)Settings.Default["MinimumDaysBetweenTests"]; }
+            set
+            {
+                if (value < 0 || value > 999)
+                    throw new Exception();
+                Settings.Default["MinimumDaysBetweenTests"] = value;
+                Properties.Settings.Default.Save();
+            }
+        }
+
+        static public int MinimumDrivingLessons
+        {
+            get { return (int)Settings.Default["MinimumDrivingLessons"]; }
+            set
+            {
+                if (value < 0 || value > 999)
+                    throw new Exception();
+                Settings.Default["MinimumDrivingLessons"] = value;
+                Properties.Settings.Default.Save();
+            }
+        }
+
+        static public int PassingGrade
+        {
+            get { return (int)Settings.Default["PassingGrade"]; }
+            set
+            {
+                if (value < 0 || value > 100)
+                    throw new Exception();
+                Settings.Default["PassingGrade"] = value;
+                Properties.Settings.Default.Save();
+            }
+        }
+
+        static public TimeSpan TestTimeSpan
+        {
+            get { return (TimeSpan)Settings.Default["TestTimeSpan"]; }
+        }
+        static public int TestTimeSpanInMinutes
+        {
+            get { return TestTimeSpan.Minutes; }
+            set
+            {
+                if (value < 0 || value > 60)
+                    throw new Exception();
+                Settings.Default["TestTimeSpan"] = new TimeSpan(0, value, 0);
+            }
+        }
+
+        static public int WorkStartHour
+        {
+            get { return (int)Settings.Default["WorkStartHour"]; }
+            set
+            {
+                if (value < 0 || value > 20 || value > WorkEndHour)
+                    throw new Exception();
+                Settings.Default["WorkStartHour"] = value;
+                Properties.Settings.Default.Save();
+            }
+        }
+
+        static public int WorkEndHour
+        {
+            get { return (int)Settings.Default["WorkEndHour"]; }
+            set
+            {
+                if (value < 5 || value > 24 || value < WorkStartHour)
+                    throw new Exception();
+                Settings.Default["WorkEndHour"] = value;
+                Properties.Settings.Default.Save();
+            }
+        }
+
+        static public int FridayWorkEndHour
+        {
+            get { return (int)Settings.Default["FridayWorkEndHour"]; }
+            set
+            {
+                if (value < 0 || value > 16)
+                    throw new Exception();
+                Settings.Default["FridayWorkEndHour"] = value;
+                Properties.Settings.Default.Save();
+            }
+        }
+
 
 
         //Email Configuration:
@@ -45,26 +175,5 @@ namespace BE
         public static string GoogleMapsApiKey = "AIzaSyBzz5Hd1UJW7NKf27JvD4HB0nBfLM4qfJQ";
         internal static string GoogleDistanceURL = "https://maps.googleapis.com/maps/api/distancematrix/";
 
-        /// <summary>
-        /// deep cloning
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="source"></param>
-        /// <returns></returns>
-        public static T Clone<T>(this T source)
-        {
-            if (Object.ReferenceEquals(source, null))
-                return default(T);
-            if (!typeof(T).IsSerializable)
-                throw new ArgumentException("The type must be serializable.", "source");
-            IFormatter formatter = new BinaryFormatter();
-            Stream stream = new MemoryStream();
-            using (stream)
-            {
-                formatter.Serialize(stream, source);
-                stream.Seek(0, SeekOrigin.Begin);
-                return (T)formatter.Deserialize(stream);
-            }
-        }
     }
 }
--- BE/Tools.cs
+++ BE/Tools.cs
@@ -11,6 +11,8 @@ using GoogleMapsApi.Entities.Directions.Response;
 using GoogleMapsApi.Entities.PlaceAutocomplete.Request;
 using System.Xml.Linq;
 using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
 //using System.Windows.Controls;
 
 namespace BE
@@ -181,6 +183,29 @@ namespace BE
             }
 
         }
+
+        /// <summary>
+        /// deep cloning
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public static T Clone<T>(this T source)
+        {
+            if (Object.ReferenceEquals(source, null))
+                return default(T);
+            if (!typeof(T).IsSerializable)
+                throw new ArgumentException("The type must be serializable.", "source");
+            IFormatter formatter = new BinaryFormatter();
+            Stream stream = new MemoryStream();
+            using (stream)
+            {
+                formatter.Serialize(stream, source);
+                stream.Seek(0, SeekOrigin.Begin);
+                return (T)formatter.Deserialize(stream);
+            }
+        }
+
 
     }
 }
