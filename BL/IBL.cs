4a23b0ca4c73caad873124699ba89528927fe690
אברהם פרנקל <avraham224@gmail.com>
2018-12-31 13:16:01 +0200

    31/12 13:20

--- BL/IBL.cs
+++ BL/IBL.cs
@@ -49,6 +49,8 @@ namespace BL
         /// </summary>
         /// <param name="test"></param>
         void UpdateTest(BE.Test test);
+        string GetEmailTemltateTestRemeinder(int TestID, string אא@);
+        void UpdateEmailSendingTime(int testID, DateTime? SummaryEmailSent = null, DateTime? RemeinderEmailSent = null);
         /// <summary>
         /// Update test results when done.
         /// </summary>
